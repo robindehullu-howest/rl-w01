f165d31955fb954dffe59e561f4a3233da5acbc9
robindehullu-howest <robin.dehullu@student.howest.be>
2024-09-23 14:40:45 +0200

    Testing basic setup

---
+++ Test.py
@@ -0,0 +1,11 @@
+import gymnasium
+
+env = gymnasium.make('CartPole-v1', render_mode="human")
+observation, info = env.reset()
+
+for _ in range(1000):
+    env.render()
+    action = env.action_space.sample()
+    observation, reward, terminated, truncated, info = env.step(action)
+    if terminated or truncated:
+        observation, info = env.reset()
